7cca5fb4e06628b8b9fa62cc14dd38437664c999
Anil Suri <anilsuri1992@gmail.com>
2020-09-20 21:15:10 +0200

    updating the file

--- CourseraDataWrangling.py
+++ CourseraDataWrangling.py
@@ -9,3 +9,10 @@ import pandas as pd
 import matplotlib.pylab as plt
 
 filename= "https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DA0101EN/auto.csv"
+
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
+         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
+         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
+         "peak-rpm","city-mpg","highway-mpg","price"]
+
+df = pd.read_csv(filename, names = headers)
